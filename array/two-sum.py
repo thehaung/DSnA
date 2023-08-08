77c3e44fc1d9dfd253f57f83c2472868ce8e234e
Hau Nguyen <thehaungdev@gmail.com>
2023-08-08 08:49:53 +0700

    chore: improvement

--- array/two-sum.py
+++ array/two-sum.py
@@ -8,8 +8,9 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         nums_dict = dict()
         for i in range(len(nums)):
-            if (target - nums[i]) in nums_dict:
-                return [nums_dict[target - nums[i]], i]
+            diff = target - nums[i]
+            if (diff) in nums_dict:
+                return [nums_dict[diff], i]
             else:
                 nums_dict[nums[i]] = i
 
