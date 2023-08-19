87e9603e5ee92f0617fc4b405e8dcf03e130d3ee
Hau Nguyen <thehaungdev@gmail.com>
2023-08-19 19:08:50 +0700

    feat: queue problems

---
+++ queue/design-circular-queue.py
@@ -0,0 +1,48 @@
+from collections import deque
+
+
+class MyCircularQueue:
+
+    def __init__(self, k: int):
+        self.queue = deque()
+        self.capacity = k
+
+    def enQueue(self, value: int) -> bool:
+        if self.capacity == len(self.queue):
+            return False
+        self.queue.append(value)
+        return True
+    # Time Complexity: O(1)
+    # Space Complexity: O(1)
+
+    def deQueue(self) -> bool:
+        if len(self.queue) == 0:
+            return False
+        self.queue.popleft()
+        return True
+    # Time Complexity: O(1)
+    # Space Complexity: O(1)
+
+    def Front(self) -> int:
+        if len(self.queue) == 0:
+            return -1
+        return self.queue[0]
+    # Time Complexity: O(1)
+    # Space Complexity: O(1)
+
+    def Rear(self) -> int:
+        if len(self.queue) == 0:
+            return -1
+        return self.queue[-1]
+    # Time Complexity: O(1)
+    # Space Complexity: O(1)
+
+    def isEmpty(self) -> bool:
+        return len(self.queue) == 0
+    # Time Complexity: O(1)
+    # Space Complexity: O(1)
+
+    def isFull(self) -> bool:
+        return len(self.queue) == self.capacity
+    # Time Complexity: O(1)
+    # Space Complexity: O(1)
