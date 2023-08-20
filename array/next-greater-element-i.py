0a09c62a17ca18e510da9a75837f87f4b2adc68c
Hau Nguyen <thehaungdev@gmail.com>
2023-08-20 17:22:10 +0700

    feat: next greater i problems with array

--- array/next-greater-element-i.py
+++ array/next-greater-element-i.py
@@ -3,4 +3,14 @@ from typing import List
 
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        return
+        numDict = dict()
+        ans = [-1] * len(nums1)
+
+        for i, num in enumerate(nums2):
+            numDict[num] = i
+        for i, num in enumerate(nums1):
+            for j in range(numDict[num], len(nums2)):
+                if nums2[j] > num:
+                    ans[i] = nums2[j]
+                    break
+        return ans
