a2f6312fad5f52ce78ae7ced1fde8a5f84a63c61
Hau Nguyen <thehaungdev@gmail.com>
2023-09-01 10:35:05 +0700

    feat: the desc for solution

--- Python/backtracking/combinations.py
+++ Python/backtracking/combinations.py
@@ -2,6 +2,17 @@ from typing import List
 
 
 class Solution:
+    """
+    Đề bài yêu cầu combine các số dựa trên n và k
+    n là max của số e.g: n = 4 thì số lớn nhất trong combine sẽ là 4
+    k là max của độ dài số sau khi combine e.g: k = 2 thì 1 số
+    chỉ được có tối đa 2 phần tử combine ví dụ [14]
+
+    Ý tưởng: Dùng backtracking để combine
+    Func backtrack sẽ nhận vào remain và nex
+    remain được tính dựa trên số k
+    nex sẽ được tính dựa trên loop range (init num, n + 1)
+    """
     def combine(self, n: int, k: int) -> List[List[int]]:
         ans = []
         current_solution = []
--- Python/recursion/min-cost-climbing-stairs.py
+++ Python/recursion/min-cost-climbing-stairs.py
@@ -3,6 +3,16 @@ from typing import List
 
 
 class Solution:
+    """
+    Đề bài yêu cầu tính cost dựa trên 1 nums array
+    Với mỗi lần bước ta có thể take 1 or 2 steps
+    Cost sẽ được cộng dồn với những ô (tương ứng với index)
+    mà ta bước qua
+
+    Ý tưởng: Ta sẽ có 2 biến tính toán, cho bước 1 bước và 2 bước khởi đầu
+    Mỗi lần bước qua 1 hoặc 2 bước ta sẽ tính min của 2 lần bước
+    Và sau đó trả ra min cost sau n lần bước
+    """
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         return self.v2CalcViaLoop(cost)
 
@@ -18,6 +28,8 @@ class Solution:
         startIndex0 = calcCost(0)
         startIndex1 = calcCost(1)
         return min(startIndex0, startIndex1)
+    # Time Complexity: O(N)
+    # Space Complexity: O(N)
 
     def v2CalcViaLoop(self, cost: List[int]) -> int:
         s1, s2 = 0, 0
@@ -25,6 +37,8 @@ class Solution:
             temp = min(s1 + cost[i - 2], s2 + cost[i - 1])
             s1, s2 = s2, temp
         return s2
+    # Time Complexity: O(N)
+    # Space Complexity: O(N)
 
 
 print(Solution().minCostClimbingStairs([10, 15, 20]))
