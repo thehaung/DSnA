b83741043c497fc062bd8539021747552949fa74
Hau Nguyen <thehaungdev@gmail.com>
2023-09-03 22:25:08 +0700

    feat: clearify problem

--- Python/binary_search/valid-perfect-square.py
+++ Python/binary_search/valid-perfect-square.py
@@ -6,11 +6,10 @@ class Solution:
     """
 
     def isPerfectSquare(self, num: int) -> bool:
-        left = 0
-        right = num
         if num == 1:
             return True
 
+        left, right = 0, num
         while left <= right:
             mid = (left + right) // 2
             if mid * mid == num:
