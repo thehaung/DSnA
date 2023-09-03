5aa4cc8bfce39ec482ea53c084de7c31470d0b17
Hau Nguyen <thehaungdev@gmail.com>
2023-09-03 22:10:43 +0700

    feat: clearify problem

--- Python/binary_search/search-a-2d-matrix.py
+++ Python/binary_search/search-a-2d-matrix.py
@@ -2,8 +2,17 @@ from typing import List
 
 
 class Solution:
+    """
+    Đề bài yêu cầu từ 1 matrix được biểu diễn trên List of List có tính monotonic
+        List[n] tăng thì List[n][m] tăng find target number
+    Ý tưởng: Binary Search
+    Có 2 cách tiếp cận nhưng vẫn base trên BS
+    1. Loop qua matrix sau đó với mỗi matrix ta dùng BS để tìm target
+    2. Ta có right = len(matrix) * len(matrix[0]) (cho là n số lượng phần tử của 1 matrix[i])
+        sau đó để tìm ra midRow và midCol dựa vào mid và n ta dùng divmod -> (mid//n, mid%n)
+    """
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        return self.v1Naive(matrix, target)
+        return self.v2Better(matrix, target)
 
     def v1Naive(self, matrix: List[List[int]], target: int) -> bool:
         for i in range(len(matrix)):
@@ -18,7 +27,6 @@ class Solution:
                 else:
                     left = mid + 1
         return False
-
     # Time Complexity: O(N * logN)
     # Space Complexity: O(N)
 
@@ -39,4 +47,4 @@ class Solution:
     # Space Complexity: O(1)
 
 
-print(Solution().searchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 0))
+print(Solution().searchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 30))
