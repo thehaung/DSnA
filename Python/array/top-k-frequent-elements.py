db901dfc64d587c35dddd4d3b7603e884a2d7808
Hau Nguyen <thehaungdev@gmail.com>
2023-09-04 15:44:42 +0700

    chore: modify function

--- Python/array/top-k-frequent-elements.py
+++ Python/array/top-k-frequent-elements.py
@@ -7,11 +7,12 @@ class Solution:
         if k == len(nums):
             return nums
         numMap = dict()
-        for _, num in enumerate(nums):
+        for num in nums:
             if num in numMap:
                 numMap[num] += 1
             else:
                 numMap[num] = 1
-        sortedNums = sorted(
-            numMap.items(), key=operator.itemgetter(1), reverse=True)
+        sortedNums = sorted(numMap.items(), key=operator.itemgetter(1), reverse=True)
         return [item[0] for item in sortedNums[:k]]
+    # Time Complexity: O(NlogN) -> NlogN is sorted function TC (Timsort)
+    # Space Complexity: O(N)
